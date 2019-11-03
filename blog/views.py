2bd31370d02ade8da6db4a12b68dd73c07b75419
jadsonlucio <jadsonaluno@hotmail.com>
2019-11-02 21:51:41 -0300

    add some css to posts page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.http import HttpResponse
+
+from .models import Postagem
 
 # Create your views here.
 
 def home_screen(request):
-    return render(request, "blog/index.html", {})
+    postagens_hoje = Postagem.objects.filter(data_publicacao__lte = timezone.now()).order_by("data_publicacao")
+
+    print(postagens_hoje)
+    return render(request, "blog/posts.html", {"postagens_hoje" : postagens_hoje})
