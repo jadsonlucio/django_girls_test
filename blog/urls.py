9977c72cf4171828e3eeb783f9a076e39c14a2c2
jadsonlucio <jadsonaluno@hotmail.com>
2019-11-02 23:36:52 -0300

    add register post system

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_screen, name="home_screen")
+    path('', views.home_screen, name="home_screen"),
+    path('posts/<int:id>', views.show_post, name = "post_show"),
+    path('create/', views.create_post, name = "create_post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.http import HttpResponse
 
 from .models import Postagem
+from .forms import PostForm
 
 # Create your views here.
 
@@ -11,3 +12,24 @@ def home_screen(request):
 
     print(postagens_hoje)
     return render(request, "blog/posts.html", {"postagens_hoje" : postagens_hoje})
+
+def show_post(request, id):
+    postagem = post = get_object_or_404(Postagem, pk=id)
+    print(postagem)
+    return render(request, "blog/post.html", {"postagem" : postagem})
+
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.autor = request.user
+            post.data_publicacao = timezone.now()
+            post.save()
+
+            return redirect('post_show', id = post.pk)
+    else:
+        form = PostForm()
+    
+    return render(request, 'blog/create_posts.html', {'form' : form})
